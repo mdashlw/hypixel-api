bc4e897b2f4f2c79b756151a965b549ae5803b23
mdashl <mdashlw@gmail.com>
2019-02-17 08:44:06 +0300

    Move to Gradle bintray plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.21"
+    id("com.jfrog.bintray") version "1.8.4"
+    `maven-publish`
 }
 
 group = "com.github.mdashl"
@@ -20,4 +22,48 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.register<Jar>("sourcesJar") {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
+
+tasks.register<Jar>("javadocJar") {
+    from(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("BintrayRelease") {
+            artifactId = "hypixel-api"
+
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_API_KEY")
+
+    setPublications("BintrayRelease")
+
+    publish = true
+
+    with(pkg) {
+        repo = "maven"
+        name = "hypixel-api"
+        desc = "Kotlin Hypixel API wrapper"
+        issueTrackerUrl = "https://github.com/mdashl/hypixel-api/issues"
+        vcsUrl = "https://github.com/mdashl/hypixel-api.git"
+        githubRepo = "https://github.com/mdashl/hypixel-api.git"
+        githubReleaseNotesFile = "README.md"
+        publicDownloadNumbers = false
+        setLicenses("MIT")
+        setLabels("Hypixel", "Kotlin")
+    }
 }
