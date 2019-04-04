70f531ea0e9166e444ed97f16c530f9c9994fd47
mdashlw <mdashlw@gmail.com>
2019-04-04 09:27:58 +0300

    [ci skip] Add suppress PropertyName for Stats class

--- src/main/kotlin/ru/mdashlw/hypixel/elements/player/stats/Stats.kt
+++ src/main/kotlin/ru/mdashlw/hypixel/elements/player/stats/Stats.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode
 import ru.mdashlw.hypixel.elements.player.stats.games.SkyWars
 import ru.mdashlw.hypixel.util.children
 
+@Suppress("PropertyName")
 class Stats(children: Map<String, JsonNode>) : ObjectNode(JsonNodeFactory.instance, children) {
     val SkyWars: SkyWars
         get() = SkyWars((get("SkyWars") as ObjectNode).children)
