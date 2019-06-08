2914d838a0b9b7c904bdecfa4143dad53117375c
Davyd Malikov <mdashlw@gmail.com>
2019-06-08 19:55:59 +0300

    5.7.4
    * Add missing UhcTitle's

--- src/main/kotlin/ru/mdashlw/hypixel/api/enums/UhcTitle.kt
+++ src/main/kotlin/ru/mdashlw/hypixel/api/enums/UhcTitle.kt
@@ -10,7 +10,12 @@ enum class UhcTitle(val star: Int, val scoreNeeded: Int, val localizedName: Stri
     CENTURION(7, 1710, "Centurion"),
     GLADIATOR(8, 2710, "Gladiator"),
     WARLORD(9, 5210, "Warlord"),
-    CHAMPION(10, 10210, "Champion");
+    CHAMPION(10, 10210, "Champion"),
+    CHAMPION_PLUS(11, 13210, "Champion"),
+    CHAMPION_PLUS_PLUS(12, 16210, "Champion"),
+    CHAMPION_PLUS_PLUS_PLUS(13, 19210, "Champion"),
+    CHAMPION_PLUS_PLUS_PLUS_PLUS(14, 22210, "Champion"),
+    HIGH_CHAMPION(15, 25210, "High Champion");
 
     override fun toString(): String = localizedName
 }
